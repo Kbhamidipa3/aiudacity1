3b7ca6fd73180105bf3fdff0620ad3a660a4b801
Kbhamidipa3 <68063553+Kbhamidipa3@users.noreply.github.com>
2020-11-23 06:33:56 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
 from sklearn.linear_model import LogisticRegression
 import argparse
 import os
@@ -9,27 +15,32 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Workspace, Dataset
 
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+# azureml-core of version 1.0.72 or higher is required
+# azureml-dataprep[pandas] of version 1.1.34 or higher is required
 
-ds = ### YOUR CODE HERE ###
 
-x, y = clean_data(ds)
+subscription_id = 'ac15aef5-0abe-4be6-a0bd-40abc1594138'
+resource_group = 'aml-quickstarts-127486'
+workspace_name = 'quick-starts-ws-127486'
 
-# TODO: Split data into train and test sets.
+workspace = Workspace(subscription_id, resource_group, workspace_name)
 
-### YOUR CODE HERE ###a
+ds = Dataset.get_by_name(workspace, name='Bank-marketing')
 
-run = Run.get_context()
+
+# In[6]:
+
 
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
     weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
-
-    # Clean and one hot encode data
+        # Clean and one hot encode data
     x_df = data.to_pandas_dataframe().dropna()
     jobs = pd.get_dummies(x_df.job, prefix="job")
     x_df.drop("job", inplace=True, axis=1)
@@ -49,16 +60,35 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    return (x_df,y_df)
     
+x, y = clean_data(ds)
+
+
+# In[7]:
+
+
+# TODO: Split data into train and test sets.
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.33,random_state=42)
+
+run = Run.get_context()
+
+
+# In[8]:
+
 
 def main():
     # Add arguments to script
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+    parser.add_argument('--max_iter', type=int, default=1000, help="Maximum number of iterations to converge")
 
-    args = parser.parse_args()
+
+    args = parser.parse_args(args=[]) #call from notebook
+
 
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
@@ -67,6 +97,8 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+    print(accuracy)
 
 if __name__ == '__main__':
     main()
+
